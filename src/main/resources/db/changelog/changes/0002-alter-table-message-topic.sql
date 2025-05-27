00fd76e60e315ef34655f6f7946c95a4b8ead05c
Éric Magalhães Souza <15831837+eric-souzams@users.noreply.github.com>
2025-05-26 21:02:33 -0300

    fix: fix changelog

--- src/main/resources/db/changelog/changes/0002-alter-table-message-topic.sql
+++ src/main/resources/db/changelog/changes/0002-alter-table-message-topic.sql
@@ -1,7 +1,4 @@
 ALTER TABLE tb_message_topic
-DROP CONSTRAINT tb_message_topic_message_id_fkey;
-
-ALTER TABLE tb_message_topic
 ADD CONSTRAINT tb_message_topic_message_id_fkey
 FOREIGN KEY (message_id)
 REFERENCES tb_messages(id)
